64c98f25af1326232d60097082b8f71ffae811a8
astro_dally <animemedia64@gmail.com>
2024-09-16 09:04:40 +0530

    updated .env

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// CORS setup
+const corsOptions = {
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Replace with your React frontend URL
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); // Serve files from 'uploads'
